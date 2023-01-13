1c560a0176ee6c79350b38161299b80d2e0a8cff
Eduardo Nicolas Fernandez <eduardonicolasfernandez1234@gmail.com>
2023-01-13 17:31:24 -0400

    feat: add endpoint to create user admin

--- authentication/api/user_viewset.py
+++ authentication/api/user_viewset.py
@@ -17,7 +17,7 @@ class UserViewSet(viewsets.ModelViewSet):
     serializer_class = UserSerializer
     
     
-    @action(detail=False, url_path='client', methods=['post'], description='Insert client')
+    @action(detail=False, url_path='client', methods=['post'], description='Insert user type client')
     def insert_client(self, request, pk=None):
         try:
             serializer = UserCreateSimpleSerializer(data=request.data)
@@ -40,7 +40,7 @@ class UserViewSet(viewsets.ModelViewSet):
         serializer = UserSerializer(user)
         return Response(serializer.data, status=status.HTTP_201_CREATED)
 
-    @action(detail=False, url_path='supervisor', methods=['post'], description='Insert supervisor')
+    @action(detail=False, url_path='supervisor', methods=['post'], description='Insert user type supervisor')
     def insert_supervisor(self, request, pk=None):
         try:
             serializer = UserCreateSimpleSerializer(data=request.data)
@@ -59,6 +59,29 @@ class UserViewSet(viewsets.ModelViewSet):
             nit=user_data['nit'],
             birth_date=user_data['birth_date'],
             user_type=User.TYPE_SUPERVISOR,
+        )
+        serializer = UserSerializer(user)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    
+    @action(detail=False, url_path='admin', methods=['post'], description='Insert user type admin')
+    def insert_admin(self, request, pk=None):
+        try:
+            serializer = UserCreateSimpleSerializer(data=request.data)
+            serializer.is_valid(raise_exception=True)
+        except Exception as e:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        
+        user_data = serializer.data
+        user = User.objects.create_user(
+            full_name=user_data['full_name'],
+            address=user_data['address'],
+            email=user_data['email'],
+            username=user_data['username'],
+            password=user_data['password'],
+            phone=user_data['phone'],
+            nit=user_data['nit'],
+            birth_date=user_data['birth_date'],
+            user_type=User.TYPE_ADMIN,
         )
         serializer = UserSerializer(user)
         return Response(serializer.data, status=status.HTTP_201_CREATED)
